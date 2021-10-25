f75bfab1f6e24a29e6c3304ea8b000aed3f993f5
Ben Armstrong <synrg@debian.org>
2021-10-25 15:06:18 -0300

    Initial core api test.

---
+++ inatcog/core/tests/inat_api_test.py
@@ -0,0 +1,32 @@
+"""Test INatAPI."""
+import json
+from unittest.mock import AsyncMock
+
+from aiohttp import web
+import pytest
+
+from ..apis.inat import INatAPI
+
+
+@pytest.fixture(name="inat_api")
+def fixture_inat_api():
+    return INatAPI()
+
+
+@pytest.fixture(name="mock_response")
+def fixture_mock_response(mocker):
+    async_mock = AsyncMock()
+    mocker.patch("aiohttp.ClientSession.get", side_effect=async_mock)
+    return async_mock
+
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestINatAPI:
+    async def test_get_taxa_by_id(self, inat_api, mock_response):
+        """Test get_taxa by id."""
+        expected_result = {"results": [{"name": "Animalia"}]}
+        mock_response.return_value = web.Response(body=json.dumps(expected_result))
+        taxon = await inat_api.get_taxa(1)
+        assert taxon["results"][0]["name"] == "Animalia"
